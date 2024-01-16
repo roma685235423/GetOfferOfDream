f55f74ba786a195b6f8ba52eb87dee03d5551aa0
Roman Boiko <roma685235423@icloud.com>
2024-01-16 22:45:29 +0300

    Frameworks added.

---
+++ getOfferCore/getOfferCore/getOfferCore.h
@@ -0,0 +1,18 @@
+//
+//  getOfferCore.h
+//  getOfferCore
+//
+//  Created by Роман Бойко on 16.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for getOfferCore.
+FOUNDATION_EXPORT double getOfferCoreVersionNumber;
+
+//! Project version string for getOfferCore.
+FOUNDATION_EXPORT const unsigned char getOfferCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <getOfferCore/PublicHeader.h>
+
+
---
+++ getOfferNetwork/getOfferNetwork/getOfferNetwork.h
@@ -0,0 +1,18 @@
+//
+//  getOfferNetwork.h
+//  getOfferNetwork
+//
+//  Created by Роман Бойко on 16.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for getOfferNetwork.
+FOUNDATION_EXPORT double getOfferNetworkVersionNumber;
+
+//! Project version string for getOfferNetwork.
+FOUNDATION_EXPORT const unsigned char getOfferNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <getOfferNetwork/PublicHeader.h>
+
+
