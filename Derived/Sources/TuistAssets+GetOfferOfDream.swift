fd71b50cac9ee324cef35403ebf736e9437a9eee
Roman Boiko <roma685235423@icloud.com>
2024-05-03 12:54:32 +0300

    Project renamed

--- Derived/Sources/TuistAssets+GetOfferOfDream.swift
+++ Derived/Sources/TuistAssets+GetOfferOfDream.swift
@@ -4,14 +4,14 @@
 // Generated using tuist — https://github.com/tuist/tuist
 
 #if os(macOS)
-import AppKit
+  import AppKit
 #elseif os(iOS)
-import UIKit
+  import UIKit
 #elseif os(tvOS) || os(watchOS)
-import UIKit
+  import UIKit
 #endif
 #if canImport(SwiftUI)
-import SwiftUI
+  import SwiftUI
 #endif
 
 // swiftlint:disable superfluous_disable_command file_length implicit_return
@@ -20,126 +20,126 @@ import SwiftUI
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 public enum GetOfferOfDreamAsset {
-    public static let accentColor = GetOfferOfDreamColors(name: "AccentColor")
-    public static let testImage1 = GetOfferOfDreamImages(name: "testImage1")
-    public static let testImage2 = GetOfferOfDreamImages(name: "testImage2")
+  public static let accentColor = GetOfferOfDreamColors(name: "AccentColor")
+  public static let testImage1 = GetOfferOfDreamImages(name: "testImage1")
+  public static let testImage2 = GetOfferOfDreamImages(name: "testImage2")
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
 // MARK: - Implementation Details
 
 public final class GetOfferOfDreamColors {
-    public fileprivate(set) var name: String
+  public fileprivate(set) var name: String
 
-    #if os(macOS)
-    public typealias Color = NSColor
-    #elseif os(iOS) || os(tvOS) || os(watchOS) || os(visionOS)
-    public typealias Color = UIColor
-    #endif
+  #if os(macOS)
+  public typealias Color = NSColor
+  #elseif os(iOS) || os(tvOS) || os(watchOS) || os(visionOS)
+  public typealias Color = UIColor
+  #endif
 
-    @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, visionOS 1.0, *)
-    public private(set) lazy var color: Color = {
-        guard let color = Color(asset: self) else {
-            fatalError("Unable to load color asset named \(name).")
-        }
-        return color
-    }()
-
-    #if canImport(SwiftUI)
-    private var _swiftUIColor: Any? = nil
-    @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, visionOS 1.0, *)
-    public private(set) var swiftUIColor: SwiftUI.Color {
-        get {
-            if self._swiftUIColor == nil {
-                self._swiftUIColor = SwiftUI.Color(asset: self)
-            }
-
-            return self._swiftUIColor as! SwiftUI.Color
-        }
-        set {
-            self._swiftUIColor = newValue
-        }
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, visionOS 1.0, *)
+  public private(set) lazy var color: Color = {
+    guard let color = Color(asset: self) else {
+      fatalError("Unable to load color asset named \(name).")
     }
-    #endif
+    return color
+  }()
 
-    fileprivate init(name: String) {
-        self.name = name
+  #if canImport(SwiftUI)
+  private var _swiftUIColor: Any? = nil
+  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, visionOS 1.0, *)
+  public private(set) var swiftUIColor: SwiftUI.Color {
+    get {
+      if self._swiftUIColor == nil {
+        self._swiftUIColor = SwiftUI.Color(asset: self)
+      }
+
+      return self._swiftUIColor as! SwiftUI.Color
     }
+    set {
+      self._swiftUIColor = newValue
+    }
+  }
+  #endif
+
+  fileprivate init(name: String) {
+    self.name = name
+  }
 }
 
 public extension GetOfferOfDreamColors.Color {
-    @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, visionOS 1.0, *)
-    convenience init?(asset: GetOfferOfDreamColors) {
-        let bundle = GetOfferOfDreamResources.bundle
-        #if os(iOS) || os(tvOS) || os(visionOS)
-        self.init(named: asset.name, in: bundle, compatibleWith: nil)
-        #elseif os(macOS)
-        self.init(named: NSColor.Name(asset.name), bundle: bundle)
-        #elseif os(watchOS)
-        self.init(named: asset.name)
-        #endif
-    }
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, visionOS 1.0, *)
+  convenience init?(asset: GetOfferOfDreamColors) {
+    let bundle = GetOfferOfDreamResources.bundle
+    #if os(iOS) || os(tvOS) || os(visionOS)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSColor.Name(asset.name), bundle: bundle)
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
 }
 
 #if canImport(SwiftUI)
 @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, visionOS 1.0, *)
 public extension SwiftUI.Color {
-    init(asset: GetOfferOfDreamColors) {
-        let bundle = GetOfferOfDreamResources.bundle
-        self.init(asset.name, bundle: bundle)
-    }
+  init(asset: GetOfferOfDreamColors) {
+    let bundle = GetOfferOfDreamResources.bundle
+    self.init(asset.name, bundle: bundle)
+  }
 }
 #endif
 
 public struct GetOfferOfDreamImages {
-    public fileprivate(set) var name: String
+  public fileprivate(set) var name: String
 
-    #if os(macOS)
-    public typealias Image = NSImage
-    #elseif os(iOS) || os(tvOS) || os(watchOS) || os(visionOS)
-    public typealias Image = UIImage
+  #if os(macOS)
+  public typealias Image = NSImage
+  #elseif os(iOS) || os(tvOS) || os(watchOS) || os(visionOS)
+  public typealias Image = UIImage
+  #endif
+
+  public var image: Image {
+    let bundle = GetOfferOfDreamResources.bundle
+    #if os(iOS) || os(tvOS) || os(visionOS)
+    let image = Image(named: name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    let image = bundle.image(forResource: NSImage.Name(name))
+    #elseif os(watchOS)
+    let image = Image(named: name)
     #endif
-
-    public var image: Image {
-        let bundle = GetOfferOfDreamResources.bundle
-        #if os(iOS) || os(tvOS) || os(visionOS)
-        let image = Image(named: name, in: bundle, compatibleWith: nil)
-        #elseif os(macOS)
-        let image = bundle.image(forResource: NSImage.Name(name))
-        #elseif os(watchOS)
-        let image = Image(named: name)
-        #endif
-        guard let result = image else {
-            fatalError("Unable to load image asset named \(name).")
-        }
-        return result
+    guard let result = image else {
+      fatalError("Unable to load image asset named \(name).")
     }
+    return result
+  }
 
-    #if canImport(SwiftUI)
-    @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, visionOS 1.0, *)
-    public var swiftUIImage: SwiftUI.Image {
-        SwiftUI.Image(asset: self)
-    }
-    #endif
+  #if canImport(SwiftUI)
+  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, visionOS 1.0, *)
+  public var swiftUIImage: SwiftUI.Image {
+    SwiftUI.Image(asset: self)
+  }
+  #endif
 }
 
 #if canImport(SwiftUI)
 @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, visionOS 1.0, *)
 public extension SwiftUI.Image {
-    init(asset: GetOfferOfDreamImages) {
-        let bundle = GetOfferOfDreamResources.bundle
-        self.init(asset.name, bundle: bundle)
-    }
+  init(asset: GetOfferOfDreamImages) {
+    let bundle = GetOfferOfDreamResources.bundle
+    self.init(asset.name, bundle: bundle)
+  }
 
-    init(asset: GetOfferOfDreamImages, label: Text) {
-        let bundle = GetOfferOfDreamResources.bundle
-        self.init(asset.name, bundle: bundle, label: label)
-    }
+  init(asset: GetOfferOfDreamImages, label: Text) {
+    let bundle = GetOfferOfDreamResources.bundle
+    self.init(asset.name, bundle: bundle, label: label)
+  }
 
-    init(decorative asset: GetOfferOfDreamImages) {
-        let bundle = GetOfferOfDreamResources.bundle
-        self.init(decorative: asset.name, bundle: bundle)
-    }
+  init(decorative asset: GetOfferOfDreamImages) {
+    let bundle = GetOfferOfDreamResources.bundle
+    self.init(decorative: asset.name, bundle: bundle)
+  }
 }
 #endif
 
